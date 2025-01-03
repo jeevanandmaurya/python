6ab4c2cf14f9f31ede1914f7a77befd8117bee5b
Jeevanand-Github <jeevanandmaurya@gmail.com>
2025-01-03 13:42:59 +0530

    commit

---
+++ chap 1/paragraph.py
@@ -0,0 +1,9 @@
+print('''
+      Python language is widely used in Machine Learning because it provides libraries like NumPy, Pandas, Scikit-learn, TensorFlow, and Keras.
+      These libraries offer tools and functions essential for data manipulation, analysis, and building machine learning models.
+      It is well-known for its readability and offers platform independence. 
+      These all things make it the perfect language of choice for Machine Learning.
+
+      Machine Learning is a subdomain of artificial intelligence. 
+      It allows computers to learn and improve from experience without being explicitly programmed,
+      and It is designed in such a way that allows systems to identify patterns, make predictions, and make decisions based on data.''')
